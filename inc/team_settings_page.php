256baebeaaedd9e5e3e493804456f00e37fb1185
Jorge <jorgelarteagap@gmail.com>
2023-09-18 19:28:17 -0400

    [FIX] Clear data for use in danzacruz

--- inc/team_settings_page.php
+++ inc/team_settings_page.php
@@ -1,5 +1,5 @@
 <div class="wrap"><div id="icon-tools" class="icon32"></div>
-	<h2>SM Team</h2>
+	<h2>STS Team</h2>
 	<div class="shortcode_info" style="background: #fff;padding: 10px 20px;">
     	<h2>Short Code:</h2>
     	<p>
@@ -9,10 +9,10 @@
             <code>[sm-member theme="3"]</code>            
         </p>
         <p>
-            <code>[sm-member theme="3"]</code>            
+            <code>[sm-member theme="2"]</code>            
         </p>
         <p>
-    		<code>[sm-member theme="3"]</code>    		
+    		<code>[sm-member theme="1"]</code>    		
     	</p>
 
     </div>
